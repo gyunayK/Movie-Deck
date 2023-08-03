e1c336690fa58eed2bb686926447ac27a731565b
OdinOfRivia <gyunay@hotmail.dk>
2023-08-03 13:43:55 -0700

    Bufix _2

--- React/client/vite.config.js
+++ React/client/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,7 @@ export default defineConfig({
     },
   },
 
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   css: {
     devSourcemap: true
   },
