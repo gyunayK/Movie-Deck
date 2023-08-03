12689b6db30513374d9a561b4302343b1150a044
OdinOfRivia <gyunay@hotmail.dk>
2023-08-03 13:31:46 -0700

    Bugfix -Tailwind not being applied in production

--- React/client/tailwind.config.js
+++ React/client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
+module.exports = {
+  purge: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
@@ -9,26 +9,13 @@ export default {
     extend: {
       screens: {
         'xs': '340px',
-
-        
         'sm': '640px',
-        // => @media (min-width: 640px) { ... }
-
         'md': '768px',
-        // => @media (min-width: 768px) { ... }
-
-     
         'lg': '1024px',
-        // => @media (min-width: 1024px) { ... }
-
         'xl': '1280px',
-        // => @media (min-width: 1280px) { ... }
-
         '2xl': '1536px',
-        // => @media (min-width: 1536px) { ... }
       }
     },
   },
   plugins: [],
 }
-
